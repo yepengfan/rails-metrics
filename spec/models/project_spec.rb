cf8c90876fd96a6a460dac23838ad2b587032dcf
Yepeng Fan <yepeng.fan@hotmail.com>
2015-12-12 22:28:41 +1100

    Write a simple rspec test case
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be an instance of Project' do
+    @project = Project.new
+    expect(@project).to be_a Project
+  end
 end
